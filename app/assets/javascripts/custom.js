ef1f1d772aa3c7ce513926093228d59236584f0b
Murilo Lucas <muhlucas@outlook.com>
2017-01-23 11:31:58 -0200

    facebookId

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,7 +2,7 @@
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.8&appId=1431166670433392";
+    js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.8&appId=230871780655102";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
