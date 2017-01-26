e3faf745513f98f53ba4f3cafe0f322b5388f580
Murilo Lucas <muhlucas@outlook.com>
2017-01-26 14:35:58 -0200

    ajuste controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,8 +9,6 @@ class PagesController < ApplicationController
     case params[:id]
       when 'sobre'
         'application_static'
-      when 'qual-a-melhor-plataforma-para-o-meu-negocio'
-        'application_static'
       else
         'application'
     end
